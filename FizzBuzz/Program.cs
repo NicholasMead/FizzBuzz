19d049dfb6259f2ab8325b14055964c37a441702
Nicholas Mead <nicholas.mead@symphonyhq.com>
2017-11-26 15:56:03 +0000

    FizzBuzz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -2,14 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace FizzBuzz
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        #region Paramaters
+        private static readonly int _speed = 5;
+        private static readonly int _startIndex = 0;
+        private static readonly int _endIndex = 500;
+        #endregion
+
+        private static void Main()
         {
+            // Pre-conditions;
+            if (_startIndex > _endIndex)
+                throw new ArgumentOutOfRangeException("Start Index cannot be less than End Index");
+            if (_speed <= 0)
+                throw new ArgumentOutOfRangeException("Speed cannot be <= 0");
+
+            // Play fizz-buzz
+            for (var x = _startIndex; x <= _endIndex; ++x)
+            {
+                Console.WriteLine(PlayFixBang(x, 3, 5));
+
+                // printout throttle
+                Thread.Sleep(1000 / _speed);
+            }
+
+            Console.ReadKey(); // keep cmd open when done...
+        }
+
+        private static string PlayFixBang(int currentInxex, int fizzIndex, int buzzIndex)
+        {
+
+            // Check Fizz
+            var responce = currentInxex % fizzIndex == 0 ? "Fizz " : "";
+
+            // Check Buzz
+            responce += currentInxex % buzzIndex == 0 ? "Buzz" : "";
+
+            // If not fizz of buzz return index, else return responce
+            return responce == "" ? currentInxex.ToString() : responce.Trim();
         }
     }
 }
